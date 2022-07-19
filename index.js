d1f4cb7a04c1137a1707f4ee52ea995134371d7a
tayefulislam <kazitayefulislam12@gmail.com>
2022-07-20 00:50:07 +0600

    initialize the project and connect to mongodb

---
+++ index.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const cors = require("cors")
+require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+
+
+
+
+const app= express()
+const port = process.env.PORT || 9000;
+
+// middleware
+
+app.use(cors());
+app.use(express.json())
+
+
+
+
+// data base
+
+const uri = process.env.URI_INFO;
+// console.log(uri)
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
+
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   client.close();
+// });
+ 
+async function run (){
+
+  await  client.connect()
+
+  const itemCollection = client.db("posZero").collection("items");
+
+
+
+}
+
+run().catch(console.log)
+
+
+
+
+
+
+
+
+
+
+app.get('/',(req,res)=>{
+    res.send("Hello")
+})
+app.listen(port,()=>{
+    console.log('server run at',port)
+})
+
